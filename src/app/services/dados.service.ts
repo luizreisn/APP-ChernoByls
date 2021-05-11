88af8732b3225f5323881649016d21def4b798bb
luizreis <luizgabrieldosreisnunes2001@gmail.com>
2021-05-11 13:18:46 -0300

    dados do usuario service

---
+++ src/app/services/dados.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+
+interface Dados{
+  id: number;
+  nome: string;
+  telefone: number;
+  cpf: number;
+  nascimento: Date;
+  sexo: string;
+  email: string;
+  senha: string;
+  dadosEndereco:{
+    cep: number;
+    endereco: string;
+    numero: number;
+    complemento: string;
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DadosService {
+
+  public usuarios: Dados[] = [];
+
+  constructor() { }
+
+  public getUsuarioId(id: number){
+    return { ...this.usuarios.find(u => u.id === id)};
+  }
+
+  public cadastrarUsuario(usuarioNovo: Dados){
+    usuarioNovo.id = 1 + Math.max(0, ...this.usuarios.map(u => u.id));
+    this.usuarios.push(usuarioNovo);
+  }
+
+  public atualizarUsuario(usuarioAtualizado: Dados){
+    const index = this.usuarios.findIndex(u => u.id === usuarioAtualizado.id);
+    this.usuarios[index] = usuarioAtualizado;
+  }
+
+  public sair(id: number){
+    const index = this.usuarios.findIndex(u => u.id === id);
+    this.usuarios.splice(index, 1);
+  }
+
+}
