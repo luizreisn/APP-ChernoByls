cdc5fed4f38baf005ee92bbbc57b122f1da25070
luizreis <luizgabrieldosreisnunes2001@gmail.com>
2021-05-19 18:40:29 -0300

    adicionando armazenamento offline

--- src/app/services/dados.service.ts
+++ src/app/services/dados.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 import { Produto, ProdutosService } from './produtos.service';
 
 export interface Dados{
@@ -28,20 +29,34 @@ export class DadosService {
 
   public usuarios: Dados[] = [];
 
-  constructor() { }
+  constructor(private storage: Storage) { 
+    this.carrregarDoStorage()
+  }
+
+  private salveNoStorage(){
+    this.storage.set('usuarios', this.usuarios)
+  }
+
+  private async carrregarDoStorage(){
+    const usuariosCarregados = await this.storage.get('usuarios')
+    this.usuarios.push(...usuariosCarregados)
+  }
 
   public getUsuarioId(id: number){
+    this.carrregarDoStorage()
     return JSON.parse(JSON.stringify(this.usuarios.find(u => u.id === id)));
   }
 
   public cadastrarUsuario(usuarioNovo: Dados){
     usuarioNovo.id = 1 + Math.max(0, ...this.usuarios.map(u => u.id));
     this.usuarios.push(usuarioNovo);
+    this.salveNoStorage();
   }
 
   public atualizarUsuario(usuarioAtualizado: Dados){
     const index = this.usuarios.findIndex(u => u.id === usuarioAtualizado.id);
     this.usuarios[index] = usuarioAtualizado;
+    this.salveNoStorage();
   }
 
   public atualizarFavorito(produto: Produto, id: number){
@@ -54,12 +69,14 @@ export class DadosService {
       usuario.produtosFavoritos.push(produto.id)
       console.log('Favorito', usuario.produtosFavoritos)
     }
+    this.salveNoStorage();
   }
 
   public adicionarNoCarrinho(id: number, prod: Produto){
     const usuario = this.usuarios.find(u => u.id === id)
     usuario.carrinho.push(JSON.parse(JSON.stringify(prod)))
     console.log(usuario.carrinho)
+    this.salveNoStorage();
   }
 
   public excluirDoCarrinho(id: number, prod: Produto){
@@ -67,11 +84,13 @@ export class DadosService {
     const index = usuario.carrinho.indexOf(prod);
     usuario.carrinho.splice(index, 1)
     console.log(usuario.carrinho)
+    this.salveNoStorage();
   }
 
   public sair(id: number){
     const index = this.usuarios.findIndex(u => u.id === id);
     this.usuarios.splice(index, 1);
+    this.salveNoStorage();
   }
 
 }
